a0a91f3480edbde9b167498fcac2bae7272a32d3
huynhngocphuc <huynhngocphuc.9d@gmail.com>
2021-01-03 21:41:47 +0700

    insert data

---
+++ src/database/insertLapTop.sql
@@ -0,0 +1,60 @@
+#laptop acer
+insert into product(product_name,image,price,detail_cate_id)
+values 
+("Laptop Acer Aspire 5","AcerAspire5.png",800,1),
+("Laptop Acer Gaming Nitro 5","AcerGamingNitro5.jpg",1000,1),
+("Laptop Acer Swift 3","AcerSwift3.jpg",850,1),
+("Laptop ACER Swift 5 ","ACERSwift5.jpg",1100,1),
+("Laptop Acer Aspire 3","AcerAspire3.jpg",1100,1),
+("Laptop ACER Aspire 5","AcerAspire5.jpg",1100,1),
+("Laptop ACER Aspire 7","AcerAspire7.jpg",990,1),
+("Laptop ACER Nitro 5","ACERNitro5.jpg",1100,1),
+("Laptop Acer Predator Helios 300","AcerPredatorHelios300.jpg",2500,1);
+#laptop asus
+insert into product(product_name,image,price,detail_cate_id)
+values 
+("Laptop Gaming Asus TUF F15","GamingAsusTUFF15.jpg",800,2),
+("Laptop Gaming Asus TUF A15","GamingAsusTUFA15.jpg",1600,2),
+("Asus ZenBook UX371EA-HL701TS","AsusZenBook.jpg",1600,2),
+("Laptop ASUS Vivobook S533EA","VivobookS533EA.jpg",1600,2),
+("Laptop ASUS ExpertBook P2451FA EK0229T","ExpertBookP2451FA.jpg",1600,2),
+("Laptop ASUS TUF Gaming FA506IU AL127T","GamingFA506IU.jpg",1600,2),
+("Laptop ASUS Vivobook Flip TM420IA EC155","vivobookFlipTM420IA.jpg",1600,2),
+("Laptop ASUS ROG Strix G15","ASUSROGStrixG15.jpg",1600,2),
+("Laptop ASUS ROG Zephyrus G15 ","ASUSROGZephyrusG15.jpg",1500,2);
+#laptopDell
+insert into product(product_name,image,price,detail_cate_id)
+values 
+("Laptop Dell Inspiron 14 3493","DellInspiron143493.jpg",500,3),
+("Laptop Dell Inspiron 5391","DellInspiron5391.jpg",500,3),
+("Laptop Dell Inspiron 5593","DellInspiron5593.jpg",500,3),
+("Laptop Dell Inspiron 7306","DellInspiron7306.jpg",500,3),
+("Laptop Dell Inspiron 15 7501","DellInspiron157501.jpg",1560,3),
+("Laptop Dell G3 15 3500","DellG3153500.jpg",500,3),
+("Laptop Dell G5 15 5500","DellG3155500.jpg",500,3),
+("Laptop Dell Vostro 15 3590","DellVostro153590.jpg",500,3),
+("Laptop Dell Vostro 5402","DellVostro5402.jpg",970,3);
+#laptop lenovo
+insert into product(product_name,image,price,detail_cate_id)
+values 
+("Lenovo ThinkBook 14IML i3","LenovoThinkBook14IMLi3.jpg",500,4),
+("Laptop Lenovo Ideapad Gaming 3 15IMH05","LenovoIdeapadGaming3.jpg",970,4),
+("Laptop Lenovo Legion 5 15IMH05","LenovoLegion515IMH05.jpg",1250,4),
+("Laptop Lenovo Ideapad L340","LenovoIdeapadL340.jpg",1250,4),
+("Laptop Lenovo Ideapad 5","LenovoIdeapad5.jpg",1250,4),
+("Laptop Lenovo Ideapad S145","LenovoIdeapadS145.jpg",1250,4),
+("Laptop Lenovo ThinkBook 13s","LenovoThinkBook13s.jpg",1250,4),
+("Laptop Lenovo ThinkBook 14","LenovoThinkBook14.jpg",1250,4),
+("Laptop Lenovo Legion Y540","LenovoLegionY540.jpg",1250,4);
+#laptop HP
+insert into product(product_name,image,price,detail_cate_id)
+values 
+("Laptop HP Pavilion 15","HPPavilion15.jpg",600,5),
+("Laptop HP Envy 13","HPEnvy13.png",1250,5),
+("Laptop HP 340s G7 240Q4PA ","HP340sG7240Q4PA.jpg",1250,5),
+("Laptop HP 340s G7 240Q3PA","HP340sG7240Q3PA.jpg",1250,5),
+("Laptop HP 340s G7 224L1PA","HP340sG7224L1PA.jpg",1250,5),
+("Laptop HP Envy 13-ba0047TU 171M8PA","HPEnvy13ba0047TU171M8PA.jpg",1250,5),
+("Laptop HP ProBook 455 G7 1A1A8PA","HPProBook455G71A1A8PA.jpg",1250,5),
+("Laptop HP ProBook 450 G7-9LA54PA 9LA54PA","HPProBook450G79LA54PA.jpg",1250,5),
+("Laptop HP 348 G7","HP348G7.jpg",754,5);
