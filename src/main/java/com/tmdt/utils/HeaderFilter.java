508f014b92834c28b984e82cf9bab3e141b1e796
NghiaBui <nghiabv120100@gmail.com>
2021-06-21 00:34:44 +0700

    Cross Domain

---
+++ src/main/java/com/tmdt/utils/HeaderFilter.java
@@ -0,0 +1,38 @@
+package com.tmdt.utils;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+@WebFilter("/*")
+public class HeaderFilter implements Filter {
+
+    public static final String POLICY = "default-src 'self'";
+
+    public HeaderFilter() {
+    }
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+            throws IOException, ServletException {
+        HttpServletResponse res = (HttpServletResponse) servletResponse;
+
+        res.setHeader("Cache-Control","no-cache, no-store, must-revalidate");
+        res.setHeader("Pragma","no-cache");
+        res.setDateHeader("Expires",0);
+
+        res.setHeader("X-XSS-Protection","1; mode=block");
+        res.setHeader("X-Content-Type-Options","nosniff");
+        res.setHeader("X-Frame-Options","DENY");
+        res.setHeader("Content-Security-Policy", HeaderFilter.POLICY);
+        filterChain.doFilter(servletRequest,servletResponse);
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
